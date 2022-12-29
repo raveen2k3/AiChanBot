d59b006c851d802c7740cf7d97ad769ff170acdc
Indrajeeth <79161058+TAMILVIP007@users.noreply.github.com>
2022-12-29 15:01:54 +0530

    Typo

--- modules/ai.py
+++ modules/ai.py
@@ -1,4 +1,4 @@
-from pyrogram import filters
+from pyrogram import filters, Client 
 from pyrogram.types import Message
 import requests
 from config import neko, BOT_ID
@@ -6,7 +6,7 @@ from config import neko, BOT_ID
 
 @neko.on_message(filters.text, group=100)
 async def ai(_, message: Message):
-    if message.reply_to_message and message.reply_to_message.from_user.id == BOT_ID
+    if message.reply_to_message and message.reply_to_message.from_user.id == BOT_ID:
         ai_gen = requests.get(f"https://apikatsu.otakatsu.studio/api/chatbot/Iseria?message={message.text}", timeout=5).json()["response"]
         print(ai_gen)
         await neko.send_message(chat_id=message.chat.id ,text=ai_gen , reply_to_message_id=message.id)
